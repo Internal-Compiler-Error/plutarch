fda8c5aeac5a1bcfaaebb7a18f3451c58abbf411
Liang Wang <liang.wang.ice@gmail.com>
2022-12-28 17:12:34 -0500

    blah

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,7 +7,13 @@ export default {
     [
       "script",
       {},
-      "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-JN26VM45TW');",
+      `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'G-JN26VM45TW');
+      `,
     ],
   ],
   lastUpdated: true,
